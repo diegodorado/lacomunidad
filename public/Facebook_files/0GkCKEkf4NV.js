3f36871185202e2ab2e988aea7613fa4000dfb13
diego <diego@diego-laptop.(none)>
2011-05-27 19:39:09 -0300

    ok

--- public/Facebook_files/0GkCKEkf4NV.js
+++
@@ -1,8 +0,0 @@
-/*1297736845,169775815*/
-
-if (window.CavalryLogger) { CavalryLogger.start_js(["qZn3d"]); }
-
-add_properties('Messaging',{getUserThreadURI:function(a){return URI(env_get('www_base')).setPath('/messages/'+a);},markAsRead:function(a){a=$A(a);new AsyncRequest().setURI('/ajax/messaging/async.php').setData({action:'markRead',tids:a}).setMethod('POST').setHandler(bagofholding).setErrorHandler(bagofholding).send();Arbiter.inform('messaging/mark-as-read',{tids:a});},markUserThreadAsRead:function(a){new AsyncRequest().setURI('/ajax/messaging/async.php').setData({action:'chatMarkRead',other_user:a}).setMethod('POST').setHandler(bagofholding).setErrorHandler(bagofholding).send();Arbiter.inform('messaging/mark-as-read',{other_user:a});}});
-var MessagingEvents=window.MessagingEvents||(function(){onloadRegister(function(){Arbiter.subscribe(PresenceMessage.getArbiterMessageType('messaging'),function(a,b){var c=copy_properties(b.obj);var event=c.event||'';delete c.type;delete c.event;MessagingEvents.inform(event,c);});});return new Arbiter();})();
-
-if (window.Bootloader) { Bootloader.done(["qZn3d"]); }
